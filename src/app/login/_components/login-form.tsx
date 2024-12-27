61d583f5711436ef8441ee0dc40aefc0180a00ec
Diovane Alves <alvdiovane@gmail.com>
2024-12-27 14:16:19 -0300

    feat: implement authentication with NextAuth and PostgreSQL, including user and session models

--- src/app/login/_components/login-form.tsx
+++ src/app/login/_components/login-form.tsx
@@ -1,10 +1,18 @@
+"use client";
+
+import { signIn } from "next-auth/react";
+
 import { Button } from "@/components/ui/button";
 import { Icons } from "@/components/ui/icons";
 
 export function LoginForm() {
   return (
     <div className="mt-8 space-y-4">
-      <Button variant="outline" className="w-full">
+      <Button
+        variant="outline"
+        className="w-full"
+        onClick={() => signIn("google", { redirectTo: "/dashboard" })}
+      >
         <Icons.google className="mr-w h-4 w-4" />
         Continue with Google
       </Button>
---
+++ src/services/auth/index.ts
@@ -0,0 +1,26 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { prisma } from "../database";
+
+export const {
+  handlers: { GET, POST },
+  auth,
+  signIn,
+  signOut,
+} = NextAuth({
+  pages: {
+    signIn: "/login",
+    signOut: "/login",
+    verifyRequest: "/login",
+    newUser: "/dashboard",
+  },
+  secret: process.env.SECRET_AUTH,
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+});
