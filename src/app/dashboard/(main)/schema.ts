ed223ea06f54984a1f821656b4cf2137d8ef94a4
Diovane Alves <alvdiovane@gmail.com>
2025-01-06 00:40:23 -0300

    refactor: adjust character limits for title and text in audio creation schema

--- src/app/dashboard/(main)/schema.ts
+++ src/app/dashboard/(main)/schema.ts
@@ -5,7 +5,7 @@ export const createAudioSchema = z.object({
     .string({ required_error: "Please provide a title for your audio" })
     .nonempty("Please provide a title for your audio")
     .min(3, { message: "Title must be at least 3 characters long" })
-    .max(100, { message: "Title must not exceed 100 characters" }),
+    .max(30, { message: "Title must not exceed 30 characters" }),
   text: z
     .string({
       required_error:
@@ -13,7 +13,7 @@ export const createAudioSchema = z.object({
     })
     .nonempty("Please provide the text you want the AI to convert into audio")
     .min(10, { message: "Text must be at least 10 characters long" })
-    .max(500, { message: "Text must not exceed 500 characters" }),
+    .max(600, { message: "Text must not exceed 600 characters" }),
   "voice-id": z
     .string({ required_error: "Please select a voice for your audio" })
     .nonempty("Please select a voice for your audio"),
