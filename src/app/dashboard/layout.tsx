fdf212eea282eb79baa6248eeffde8cc96df67bf
Diovane Alves <alvdiovane@gmail.com>
2025-02-14 20:20:25 -0300

    feat: wrap layout component with SessionProvider for authentication context

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -5,18 +5,21 @@ import { Toaster } from "@/components/ui/toaster";
 import { MainSidebar } from "./_components/main-sidebar";
 
 import { auth } from "@/services/auth";
+import { SessionProvider } from "next-auth/react";
 
 export default async function Layout({ children }: PropsWithChildren) {
   const session = await auth();
 
   return (
-    <SidebarProvider>
-      <MainSidebar user={session?.user} />
-      <main className="w-full">
-        <SidebarTrigger className="absolute ml-1 mt-2" />
-        {children}
-      </main>
-      <Toaster />
-    </SidebarProvider>
+    <SessionProvider session={session}>
+      <SidebarProvider>
+        <MainSidebar user={session?.user} />
+        <main className="w-full">
+          <SidebarTrigger className="absolute ml-1 mt-2" />
+          {children}
+        </main>
+        <Toaster />
+      </SidebarProvider>
+    </SessionProvider>
   );
 }
